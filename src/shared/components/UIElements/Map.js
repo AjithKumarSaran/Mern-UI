bad438d32c01818a25f0455da9ddbd7cbb569fa8
Ajith <ajith.sekar@objectfrontier.com>
2021-05-23 12:46:14 +0530

    First commit

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react';
+import ReactDOM from 'react-dom';
+
+
+import './Map.css';
+
+const Map = props => {
+    
+    return <p>hello</p>
+}
+
+export default Map;
