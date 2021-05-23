7eaf782efe32e9469a1f835632bd0abb6e3d8393
Ajith <ajith.sekar@objectfrontier.com>
2021-05-23 13:22:15 +0530

    Updates

---
+++ precache-manifest.0c45af4830d5fb1052a8e4c6f46219bd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08e98484e5864dd42afc87ec566beb4",
+    "url": "/Mern-UI/index.html"
+  },
+  {
+    "revision": "c5e72b81dac18f4e705f",
+    "url": "/Mern-UI/static/css/0.37889d7d.chunk.css"
+  },
+  {
+    "revision": "e960b2f302e422749def",
+    "url": "/Mern-UI/static/css/4.ea30b4e8.chunk.css"
+  },
+  {
+    "revision": "71395617323afbed06c4",
+    "url": "/Mern-UI/static/css/5.1d161cd5.chunk.css"
+  },
+  {
+    "revision": "7b276c8a9c8f36f9077e",
+    "url": "/Mern-UI/static/css/6.2d60eb3b.chunk.css"
+  },
+  {
+    "revision": "a29071ea4b72e6d1cebf",
+    "url": "/Mern-UI/static/css/7.3dc8beac.chunk.css"
+  },
+  {
+    "revision": "7cdfb9a9ab68938ebeef",
+    "url": "/Mern-UI/static/css/8.c2f35c7f.chunk.css"
+  },
+  {
+    "revision": "4b1dc73d8f0ada0eb2de",
+    "url": "/Mern-UI/static/css/main.6cf71d8a.chunk.css"
+  },
+  {
+    "revision": "c5e72b81dac18f4e705f",
+    "url": "/Mern-UI/static/js/0.ab08f4f0.chunk.js"
+  },
+  {
+    "revision": "2ce5894a09150698b2e3",
+    "url": "/Mern-UI/static/js/3.1d5d32d4.chunk.js"
+  },
+  {
+    "revision": "e960b2f302e422749def",
+    "url": "/Mern-UI/static/js/4.046c869b.chunk.js"
+  },
+  {
+    "revision": "71395617323afbed06c4",
+    "url": "/Mern-UI/static/js/5.c934225e.chunk.js"
+  },
+  {
+    "revision": "7b276c8a9c8f36f9077e",
+    "url": "/Mern-UI/static/js/6.49dafe55.chunk.js"
+  },
+  {
+    "revision": "a29071ea4b72e6d1cebf",
+    "url": "/Mern-UI/static/js/7.eae9d766.chunk.js"
+  },
+  {
+    "revision": "7cdfb9a9ab68938ebeef",
+    "url": "/Mern-UI/static/js/8.f846dfa5.chunk.js"
+  },
+  {
+    "revision": "4b1dc73d8f0ada0eb2de",
+    "url": "/Mern-UI/static/js/main.f1ce3906.chunk.js"
+  },
+  {
+    "revision": "86284edb11c252502c9d",
+    "url": "/Mern-UI/static/js/runtime-main.91208be3.js"
+  }
+]);
